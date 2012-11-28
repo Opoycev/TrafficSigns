0d9629448111b4338cb9ce73d3e4d2857d3080b5
Kirill Muratov <kirill.muratov@outlook.com>
2012-11-28 16:20:51 +0400

    First commit
    Добавлены файлы проекта.
    Реализовано добаление дорожных знаков на карту.
    Добавлены сторонние библиотеки.

---
+++ data.js
@@ -0,0 +1,18 @@
+var signs = [
+    {
+        name: "2.1",
+        img: "res/img/2.1.gif"
+    },
+    {
+        name: "2.4",
+        img: "res/img/2.4.gif"
+    },
+    {
+        name: "2.5",
+        img: "res/img/2.5.gif"
+    },
+    {
+        name: "3.24.6",
+        img: "res/img/3.24.6.gif"
+    }
+];
